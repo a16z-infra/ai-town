657c44d50e91aeff92e5ad13ad2195be2e583fce
Pierre Poupin <pierrep@theodo.fr>
2018-06-18 23:48:53 +0200

    Progressively migrate to TypeScript

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,27 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const AwesomeTypescriptLoader = require('awesome-typescript-loader');
 
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+      { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+    ],
+  },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Phaser3 Simple RPG',
       template: './src/index.html',
     }),
-    new CopyWebpackPlugin([ { from: 'assets', to: 'assets' } ])
+    new CopyWebpackPlugin([{ from: 'assets', to: 'assets' }]),
   ],
   output: {
     filename: 'bundle.js',
