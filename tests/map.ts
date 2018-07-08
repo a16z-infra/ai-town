a21c855aeda4d74d2815370fe61ca997354c1303
Pierre Poupin <pierrep@theodo.fr>
2018-07-09 00:36:12 +0300

    Add test to check that zones have a valid property scene

--- tests/map.ts
+++ tests/map.ts
@@ -1,4 +1,6 @@
 const maps = require('../src/constants/maps').default;
+const scenes = require('../src/constants/scenes').default;
+const sceneNames = Object.values(scenes);
 
 const MANDATORY_LAYERS = ['terrain', 'deco', 'bridge', 'treants', 'npcs', 'zones'];
 
@@ -11,7 +13,18 @@ describe('map', () => {
           const hasTerrain = map.layers.find(o => o.name === layerName);
           expect(hasTerrain).toBeDefined();
         });
-      })
+      });
+
+      describe('zones', () => {
+        it('should have a propery "scene" that points to an existing scene', () => {
+          const zonesLayer = map.layers.find(o => o.name === 'zones');
+          zonesLayer.objects.map(zone => {
+            const destination = zone.properties.scene;
+            const isDestinationInScenes = sceneNames.includes(destination);
+            expect(isDestinationInScenes).toEqual(true);
+          });
+        });
+      });
     });
   });
 });
