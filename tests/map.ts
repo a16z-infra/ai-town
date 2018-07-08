8d8975496ce8a34a2ef4762f8f833807092bf830
Pierre Poupin <pierrep@theodo.fr>
2018-07-09 00:28:01 +0300

    Test that necessary layers are present

--- tests/map.ts
+++ tests/map.ts
@@ -1,13 +1,17 @@
 const maps = require('../src/constants/maps').default;
 
+const MANDATORY_LAYERS = ['terrain', 'deco', 'bridge', 'treants', 'npcs', 'zones'];
+
 describe('map', () => {
   Object.keys(maps).map(mapInfoKey => {
     const map = require(`../assets/${maps[mapInfoKey].file}`);
     describe(mapInfoKey, () => {
-      it('should have the proper layers', () => {
-        const hasTerrain = map.layers.find(o => o.name === 'terrain');
-        expect(hasTerrain).toBeDefined();
-      });
+      MANDATORY_LAYERS.map(layerName => {
+        it(`should have the ${layerName} layer`, () => {
+          const hasTerrain = map.layers.find(o => o.name === layerName);
+          expect(hasTerrain).toBeDefined();
+        });
+      })
     });
   });
 });
