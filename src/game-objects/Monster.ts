3c6968367967e41e66b415bee422a6cd3d5bbef0
Pierre Poupin <pierrep@theodo.fr>
2018-08-17 21:59:47 +0200

    Make monsters wander around

--- src/game-objects/Monster.ts
+++ src/game-objects/Monster.ts
@@ -4,13 +4,18 @@ import { ASSETS } from '../constants/assets';
 const DESTROY_SPRITE_ATTACK_DELAY = 200;
 
 export abstract class Monster extends Character {
+  private static WANDER_DELAY = () => (1000 + 1000 * Math.random());
+  private static WANDER_LENGTH = () => (1000 + 5000 * Math.random());
+
   protected abstract WALK_ANIMATION;
   protected abstract MONSTER_IDLE_DOWN;
   protected MONSTER_SPEED = 20;
   protected MONSTER_HIT_DELAY = 100;
+  protected CHASING_DISTANCE = 100;
 
   protected hp: number;
   private chasingPlayerTimerEvent: Phaser.Time.TimerEvent;
+  private isWandering = false;
 
   public updateMonster() {
     if (!this.active) {
@@ -60,7 +65,7 @@ export abstract class Monster extends Character {
     const monsterPoint = this.getCenter();
     const distance = monsterPoint.distance(playerPoint);
 
-    if (distance < 100) {
+    if (distance < this.CHASING_DISTANCE) {
       return true;
     }
 
@@ -75,7 +80,7 @@ export abstract class Monster extends Character {
     return x < 0 ? 'left' : 'right';
   }
 
-  private moveMonster() {
+  private moveTowardsPlayer() {
     if (!this.active) {
       return;
     }
@@ -83,6 +88,18 @@ export abstract class Monster extends Character {
     const playerPoint = this.scene.player.getCenter();
     const monsterPoint = this.getCenter();
     const { x, y } = playerPoint.subtract(monsterPoint);
+
+    this.run(x, y);
+  }
+
+  private run(x: number, y: number) {
+    if (x === 0 && y === 0) {
+      return;
+    }
+
+    if (!this.active) {
+      return;
+    }
 
     this.setVelocityX(Math.sign(x) * this.MONSTER_SPEED);
     this.setVelocityY(Math.sign(y) * this.MONSTER_SPEED);
@@ -92,10 +109,19 @@ export abstract class Monster extends Character {
     this.animate(this.WALK_ANIMATION, orientation);
   }
 
+  private stopRunning() {
+    if (!this.active) {
+      return;
+    }
+
+    this.setVelocity(0);
+    this.beIdle();
+  }
+
   private startChasing() {
     this.chasingPlayerTimerEvent = this.scene.time.addEvent({
       delay: 500,
-      callback: this.moveMonster,
+      callback: this.moveTowardsPlayer,
       callbackScope: this,
       repeat: Infinity,
       startAt: 2000,
@@ -108,8 +134,7 @@ export abstract class Monster extends Character {
 
   private stopChasing() {
     if (this.active) {
-      this.setVelocity(0);
-      this.beIdle();
+      this.stopRunning();
     }
     this.chasingPlayerTimerEvent.destroy();
     this.chasingPlayerTimerEvent = null;
@@ -118,10 +143,54 @@ export abstract class Monster extends Character {
   private handleChase() {
     if (!this.chasingPlayerTimerEvent && this.shouldChase()) {
       this.startChasing();
+      return;
     }
 
     if (this.chasingPlayerTimerEvent && !this.shouldChase()) {
       this.stopChasing();
     }
+
+    if (!this.shouldChase()) {
+      this.wanderAround();
+    }
+  }
+
+  private wanderAround() {
+    if (this.isWandering) {
+      return;
+    }
+
+    this.isWandering = true;
+
+    const direction = this.getRandomDirection();
+    this.run(direction.x, direction.y);
+
+    this.scene.time.addEvent({
+      delay: Monster.WANDER_LENGTH(),
+      callbackScope: this,
+      callback: () => {
+        this.stopRunning();
+
+        if (!this.active) {
+          return;
+        }
+
+        this.scene.time.addEvent({
+          delay: Monster.WANDER_DELAY(),
+          callbackScope: this,
+          callback: () => {
+            this.isWandering = false;
+          },
+        });
+      },
+    });
+  }
+
+  private getRandomDirection() {
+    const randomBetweenMinusOneAndOne = () => Math.round(2 * Math.random()) - 1;
+    const x = randomBetweenMinusOneAndOne();
+    const y = randomBetweenMinusOneAndOne();
+
+    return { x, y };
   }
 }
