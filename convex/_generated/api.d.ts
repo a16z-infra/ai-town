94cf7f394e1e7d462115cd9d189194bfae81b3bc
Ian Macartney <ianhmacartney@gmail.com>
2023-08-07 20:19:41 -0700

    add init

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as init from "../init";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -23,7 +24,9 @@ import type {
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
-declare const fullApi: ApiFromModules<{}>;
+declare const fullApi: ApiFromModules<{
+  init: typeof init;
+}>;
 export declare const api: FilterApi<
   typeof fullApi,
   FunctionReference<any, "public">
---
+++ convex/init.ts
@@ -0,0 +1,7 @@
+import { internalMutation } from './_generated/server';
+export default internalMutation({
+  args: {},
+  handler: async (ctx, args) => {
+    console.log('You are in reset town. There is nothing here');
+  },
+});
