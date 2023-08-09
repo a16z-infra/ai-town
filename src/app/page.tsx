7f667ee617fb87494b94d74da0fbbc605683f0fd
Nicolas Ettlin <nicolas@convex.dev>
2023-08-09 12:08:00 -0700

    Add background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 import Navbar from '@/components/Navbar';
 import Examples from '@/components/Examples';
 
+import background from '../../assets/u';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between font-body game-background">
       <Navbar />
-      <div className="w-full min-h-screen relative isolate overflow-hidden bg-gray-900 px-6 py-24 shadow-2xl sm:px-24 xl:py-32 flex flex-col justify-center">
-        <h1 className="mx-auto max-w-2xl text-center text-6xl sm:text-8xl font-bold font-display leading-none tracking-wide game-title">
+      <div className="w-full min-h-screen relative isolate overflow-hidden px-6 py-24 shadow-2xl sm:px-24 xl:py-32 flex flex-col justify-center">
+        <h1 className="mx-auto text-center text-6xl sm:text-8xl font-bold font-display leading-none tracking-wide game-title">
           AI Town
         </h1>
 
-        <p className="mx-auto mt-4 max-w-xl text-center text-xl leading-8 text-white">
+        <p className="mx-auto my-4 text-center text-xl sm:text-2xl text-white leading-tight">
           A virtual town where AI characters live, chat and socialize.
         </p>
 
