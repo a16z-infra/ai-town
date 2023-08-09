642f170bffcabc572328762db5a4229e059791b5
Nicolas Ettlin <nicolas@convex.dev>
2023-08-09 16:52:28 -0700

    Fix size issues

--- src/components/Examples.tsx
+++ src/components/Examples.tsx
@@ -13,7 +13,7 @@ export default function Examples() {
   console.log({ width, height });
 
   return (
-    <div className="mx-auto flex-1 w-full max-w mt-7 grid grid-rows-[240px_1fr] lg:grid-rows-[1fr] lg:grid-cols-[1fr_auto] lg:max-h-[900px] max-w-[1400px] min-h-[480px] game-frame">
+    <div className="mx-auto w-full max-w mt-7 grid grid-rows-[240px_1fr] lg:grid-rows-[1fr] lg:grid-cols-[1fr_auto] lg:h-[700px] max-w-[1400px] min-h-[480px] game-frame">
       {/* Game area */}
       <div className="relative overflow-hidden bg-brown-900" ref={gameWrapperRef}>
         <div className="absolute inset-0">
