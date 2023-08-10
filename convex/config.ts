9b1f4bafad54eccb7a87df552196e3d7e2c59b09
Ian Macartney <ianhmacartney@gmail.com>
2023-08-09 21:22:27 -0700

    comments on configs

--- convex/config.ts
+++ convex/config.ts
@@ -1,17 +1,18 @@
 import { Pose } from './schema.js';
 
+// How close a player can be to have a conversation.
 export const NEARBY_DISTANCE = 3;
 // Close enough to stop and observe something.
 export const CLOSE_DISTANCE = 1;
+// How long it takes a player to walk one tile.
 export const TIME_PER_STEP = 2_000;
 // After this many ms, give up on the agent and start thinking again.
 export const AGENT_THINKING_TOO_LONG = 60_000;
 // If you don't set a start position, you'll start at 0,0.
 export const DEFAULT_START_POSE: Pose = { position: { x: 0, y: 0 }, orientation: 0 };
-export const CONVERSATION_DEAD_THRESHOLD = 600_000; // In ms
-
+// How often to send up heartbeats
 export const HEARTBEAT_PERIOD = 30_000; // In ms
-
+// How long to wait after heartbeats before considering a world idle.
 export const WORLD_IDLE_THRESHOLD = 300_000; // In ms
 // How long to wait before update a memory's last access time.
 export const MEMORY_ACCESS_THROTTLE = 300_000; // In ms
