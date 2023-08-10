0616da01a4d92ad83431cc3b31e0dc48ef996a4e
Ian Macartney <ianhmacartney@gmail.com>
2023-08-09 20:58:28 -0700

    adjust config

--- convex/config.ts
+++ convex/config.ts
@@ -2,10 +2,10 @@ import { Pose } from './schema.js';
 
 export const NEARBY_DISTANCE = 3;
 // Close enough to stop and observe something.
-export const CLOSE_DISTANCE = 2;
-export const TIME_PER_STEP = 1_000;
+export const CLOSE_DISTANCE = 1;
+export const TIME_PER_STEP = 2_000;
 // After this many ms, give up on the agent and start thinking again.
-export const AGENT_THINKING_TOO_LONG = 660_000;
+export const AGENT_THINKING_TOO_LONG = 60_000;
 // If you don't set a start position, you'll start at 0,0.
 export const DEFAULT_START_POSE: Pose = { position: { x: 0, y: 0 }, orientation: 0 };
 export const CONVERSATION_DEAD_THRESHOLD = 600_000; // In ms
