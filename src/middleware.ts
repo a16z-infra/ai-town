44b28588c5aa617fc15c77ffabb3bd9c68b82f23
Nicolas Ettlin <nicolas@convex.dev>
2023-08-10 14:14:40 -0700

    Fix Clerk conflict

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,7 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware } from '@clerk/nextjs';
 
-export default authMiddleware({
-  publicRoutes: ["(.*)"],
-});
+export default authMiddleware({});
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
