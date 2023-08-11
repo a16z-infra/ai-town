fbb09b0074d062104fd997512a9d845145a29646
Nicolas Ettlin <nicolas@convex.dev>
2023-08-11 15:32:34 -0700

    Support load balancing on the front end

--- src/app/ConvexClientProvider.tsx
+++ src/app/ConvexClientProvider.tsx
@@ -2,7 +2,34 @@
 import { ReactNode } from 'react';
 import { ConvexProvider, ConvexReactClient } from 'convex/react';
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL ?? '');
+/**
+ * Determines the Convex deployment to use.
+ *
+ * We perform load balancing on the frontend, by randomly selecting one of the available instances.
+ * We use localStorage so that individual users stay on the same instance.
+ */
+function convexUrl(): string {
+  const urlsString = process.env.NEXT_PUBLIC_CONVEX_URLS;
+  if (!urlsString) {
+    const url = process.env.NEXT_PUBLIC_CONVEX_URL;
+    if (!url) {
+      throw new Error('Couldn’t find the Convex deployment URL.');
+    }
+    return url;
+  }
+
+  const urls = urlsString.split('\n');
+  const activeUrl = localStorage.getItem('convexDeploymentUrl');
+  if (activeUrl && urls.includes(activeUrl)) {
+    return activeUrl;
+  }
+
+  const newUrl = urls[Math.floor(Math.random() * urls.length)].trim();
+  localStorage.setItem('convexDeploymentUrl', newUrl);
+  return newUrl;
+}
+
+const convex = new ConvexReactClient(convexUrl());
 
 export default function ConvexClientProvider({ children }: { children: ReactNode }) {
   return <ConvexProvider client={convex}>{children}</ConvexProvider>;
