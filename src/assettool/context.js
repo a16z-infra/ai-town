25626550f97185e93c97b36bc9072c709b191a88
61cygni <>
2023-08-27 16:11:51 -0700

    Checking in aux files.

---
+++ src/assettool/context.js
@@ -0,0 +1,26 @@
+import * as PIXI from 'pixi.js'
+
+var ContextCreate = (function(){
+
+    function ContextSingleton() {
+        this.tile_index = 0;
+        this.selected_tiles = [];
+        this.tileDim = 32; // px
+        this.dimlog = Math.log2(this.tileDim); 
+    }
+
+    var instance;
+    return {
+        getInstance: function(){
+            if (instance == null) {
+                instance = new ContextSingleton();
+                // Hide the constructor so the returned object can't be new'd...
+                instance.constructor = null;
+            }
+            return instance;
+        }
+   };
+})();
+
+// global shared state between all panes
+export let g_context = ContextCreate.getInstance();
