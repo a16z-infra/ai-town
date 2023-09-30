47da4777bd77a43745a0597d062f7721e2e17eb6
Sujay Jayakar <sujayakar@convex.dev>
2023-09-30 11:58:08 -0500

    remove compression

--- convex/engine/historicalTable.ts
+++ convex/engine/historicalTable.ts
@@ -266,6 +266,8 @@ export function packSampleRecord(
   fields: NormalizedFieldConfig,
   sampleMap: Record<FieldName, History>,
 ): ArrayBuffer {
+  return new TextEncoder().encode(JSON.stringify(sampleMap)).buffer;
+
   // Pack the field configuration into a buffer and compute a four byte hash.
   const out = new ArrayBuffer(65536);
   const outView = new DataView(out);
@@ -334,6 +336,8 @@ export function packSampleRecord(
 }
 
 export function unpackSampleRecord(fields: FieldConfig, buffer: ArrayBuffer) {
+  return JSON.parse(new TextDecoder().decode(buffer));
+
   const view = new DataView(buffer);
   let pos = 0;
 
