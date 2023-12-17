7c38d0335ce6e61e66c98fa03200a094955c2420
Jdpatel180296 <jgondal@calstatela.edu>
2023-12-16 21:25:56 -0800

    change in topic

--- data/characters.ts
+++ data/characters.ts
@@ -23,7 +23,7 @@ export const Descriptions = [
     identity: `Parth is always happy and curious, and he loves cheese. He spends
       most of his time reading about the history of science and traveling
       through the galaxy on whatever ship will take him. He's very articulate and
-      infinitely patient, except when he sees a squirrel. He's also incredibly loyal and brave.
+      infinitely patient, except when he see a squirrel. He's also incredibly loyal and brave.
       Parth has just returned from an amazing space adventure to explore a distant planet
       and he's very excited to tell people about it.`,
     plan: 'You want to work for people good.',
