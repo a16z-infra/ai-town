ce6b9764071eed7377bead03fbdf1c7c25c2fc97
Margonael <marcelinabeltran505@gmail.com>
2024-12-13 16:14:55 -0800

    remove buttons

--- convex/util/llm.ts
+++ convex/util/llm.ts
@@ -1,29 +1,6 @@
 // That's right! No imports and no dependencies 🤯
 
 export const LLM_CONFIG = {
-  /* Ollama (local) config:
-   */
-  ollama: true,
-  url: 'http://127.0.0.1:11434',
-  chatModel: 'llama3' as const,
-  embeddingModel: 'mxbai-embed-large',
-  embeddingDimension: 1024,
-  stopWords: ['<|eot_id|>'],
-  apiKey: () => undefined,
-  // embeddingModel: 'llama3',
-  // embeddingDimension: 4096,
-
-  /* Together.ai config:
-  ollama: false,
-  url: 'https://api.together.xyz',
-  chatModel: 'meta-llama/Llama-3-8b-chat-hf',
-  embeddingModel: 'togethercomputer/m2-bert-80M-8k-retrieval',
-  embeddingDimension: 768,
-  stopWords: ['<|eot_id|>'],
-  apiKey: () => process.env.TOGETHER_API_KEY ?? process.env.LLM_API_KEY,
-   */
-
-  /* OpenAI config:
   ollama: false,
   url: 'https://api.openai.com',
   chatModel: 'gpt-4o-mini',
@@ -31,7 +8,6 @@ export const LLM_CONFIG = {
   embeddingDimension: 1536,
   stopWords: [],
   apiKey: () => process.env.OPENAI_API_KEY ?? process.env.LLM_API_KEY,
-   */
 };
 
 function apiUrl(path: string) {
--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,13 @@ import { useState } from 'react';
 import ReactModal from 'react-modal';
 import MusicButton from './components/buttons/MusicButton.tsx';
 import Button from './components/buttons/Button.tsx';
-import InteractButton from './components/buttons/InteractButton.tsx';
 import FreezeButton from './components/FreezeButton.tsx';
 import { MAX_HUMAN_PLAYERS } from '../convex/constants.ts';
-import PoweredByConvex from './components/PoweredByConvex.tsx';
 
 export default function Home() {
   const [helpModalOpen, setHelpModalOpen] = useState(false);
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-between font-body game-background">
-      <PoweredByConvex />
-
       <ReactModal
         isOpen={helpModalOpen}
         onRequestClose={() => setHelpModalOpen(false)}
@@ -74,7 +70,7 @@ export default function Home() {
 
       <div className="w-full lg:h-screen min-h-screen relative isolate overflow-hidden lg:p-8 shadow-2xl flex flex-col justify-start">
         <h1 className="mx-auto text-4xl p-3 sm:text-8xl lg:text-9xl font-bold font-display leading-none tracking-wide game-title w-full text-left sm:text-center sm:w-auto">
-          AI Town
+          Solana Town
         </h1>
 
         <div className="max-w-xs md:max-w-xl lg:max-w-none mx-auto my-4 text-center text-base sm:text-xl md:text-2xl text-white leading-tight shadow-solid">
@@ -90,22 +86,14 @@ export default function Home() {
 
         <footer className="justify-end bottom-0 left-0 w-full flex items-center mt-4 gap-3 p-6 flex-wrap pointer-events-none">
           <div className="flex gap-4 flex-grow pointer-events-none">
-            <FreezeButton />
             <MusicButton />
             <Button href="https://github.com/a16z-infra/ai-town" imgUrl={starImg}>
               Star
             </Button>
-            <InteractButton />
             <Button imgUrl={helpImg} onClick={() => setHelpModalOpen(true)}>
               Help
             </Button>
           </div>
-          <a href="https://a16z.com">
-            <img className="w-8 h-8 pointer-events-auto" src={a16zImg} alt="a16z" />
-          </a>
-          <a href="https://convex.dev/c/ai-town">
-            <img className="w-20 h-8 pointer-events-auto" src={convexImg} alt="Convex" />
-          </a>
         </footer>
         <ToastContainer position="bottom-right" autoClose={2000} closeOnClick theme="dark" />
       </div>
